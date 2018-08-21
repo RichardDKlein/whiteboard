bd9b8d96dd0190bea304aa19c4d250214fe75985
Richard Klein <rklein@mobileiron.com>
2018-08-21 13:39:42 -0700

    Add comments to SalesTerritories.java

--- src/graphs/SalesTerritories.java
+++ src/graphs/SalesTerritories.java
@@ -4,10 +4,6 @@ import javafx.util.Pair;
 
 import java.util.*;
 
-// @TODO: ***************************************************
-// @TODO: *************** ADD COMMENTS **********************
-// @TODO: ***************************************************
-
 /**
  * Given a list of city pairs, where each pair denotes that
  * the two cities belong to the same sales territories, find
@@ -16,25 +12,96 @@ import java.util.*;
  */
 public class SalesTerritories {
 
+    /**
+     * Helper class representing a node in the graph of cities.
+     */
     private class CityNode {
+        /**
+         * The name of the city represented by this node.
+         */
         String city;
+
+        /**
+         * The set of immediate neighbors of this node.
+         */
         Set<CityNode> neighbors = new HashSet<>();
+
+        /**
+         * A flag indicating whether this node has already been
+         * visited. (Used to avoid visiting a node twice during
+         * graph traversal.)
+         */
         boolean visited = false;
 
+        /**
+         * Constructor.
+         *
+         * @param city The name of the city represented by this
+         *             node.
+         */
         CityNode(String city) {
             this.city = city;
         }
     }
 
+    /**
+     * The set of city pairs whose sales territories we are being
+     * asked to discover.
+     */
     private Set<Pair<String, String>> cityPairs;
+
+    /**
+     * A graph showing the relationships between the cities. Each
+     * node in the graph represents one city. Each edge between
+     * two city nodes represents that both cities are in the same
+     * sales territory.
+     */
     private Set<CityNode> cityNodes = new HashSet<>();
+
+    /**
+     * A map that maps each city's name to its node in the city
+     * graph.
+     */
     private Map<String, CityNode> cityMap = new HashMap<>();
+
+    /**
+     * The set of sales territories. Each territory is a set
+     * containing the names of the cities belonging to that
+     * territory.
+     */
     private Set<Set<String>> territories = new HashSet<>();
 
+    /**
+     * Constructor.
+     *
+     * @param cityPairs The pairs of cities whose sales territories
+     *                  we are being asked to discover.
+     */
     public SalesTerritories(Set<Pair<String, String>> cityPairs) {
         this.cityPairs = cityPairs;
     }
 
+    /**
+     * Given a list of city pairs, where each pair denotes that
+     * the two cities belong to the same sales territories, find
+     * all the territories, i.e. find all the cities composing
+     * each territory.
+     *
+     * This is a straightforward graph problem. Represent the
+     * cities, and the relationships between them, by a graph.
+     * Each node represents one city. Each edge connecting two
+     * cities represents that both cities are in the same sales
+     * territory.
+     *
+     * Finding the sales territories amounts to finding all the
+     * connected subgraphs of cities. We do this with a simple
+     * Breadth-First Search (BFS).
+     *
+     * Running time is O(n).
+     *
+     * @return The given cities, organized into sets representing
+     * their sales territories.
+     */
     public Set<Set<String>> findSalesTerritories() {
         buildCityGraph();
         for (CityNode cityNode : cityNodes) {
@@ -47,6 +114,9 @@ public class SalesTerritories {
         return territories;
     }
 
+    /**
+     * From the given pairs of city names, build the city graph.
+     */
     private void buildCityGraph() {
         cityNodes.clear();
         cityMap.clear();
@@ -60,6 +130,21 @@ public class SalesTerritories {
         }
     }
 
+    /**
+     * Helper method to find or create a node in the city graph.
+     *
+     * @param city The city name of the node to find or create.
+     *             If a node for that city already exists in the
+     *             city graph, that node will be returned.
+     *
+     *             If a node for that city does *not* already
+     *             exist, it will be created, and added to the
+     *             city graph. In addition, an entry will be
+     *             added to the map of city names to city nodes.
+     *             The new city node will be returned.
+     *
+     * @return The city node found or created.
+     */
     private CityNode findOrCreateNode(String city) {
         CityNode node = cityMap.get(city);
         if (node == null) {
@@ -70,6 +155,18 @@ public class SalesTerritories {
         return node;
     }
 
+    /**
+     * Helper method to add a city, and its entire connected
+     * subgraph of cities, to a given territory.
+     *
+     * @param root The city node that we are to take as the
+     *             starting point to find the entire connected
+     *             subgraph of city nodes.
+     *
+     * @param territory The territory to which we are to add
+     *                  all the connected cities that we find
+     *                  (including the starting 'root' city).
+     */
     private void addConnectedCitiesToTerritory(CityNode root, Set<String> territory) {
         // Breadth-First Search (BFS)
         Queue<CityNode> nodeQueue = new LinkedList<>();
