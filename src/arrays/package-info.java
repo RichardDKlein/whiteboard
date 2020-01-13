cafbdf3e5ec5a8322a821e79e95330c5ddb4977f
Richard Klein <rklein@mobileiron.com>
2020-01-12 16:32:40 -0800

    Added arrays/package-info.java
    Contains hints for solving the array problems.

---
+++ src/arrays/package-info.java
@@ -0,0 +1,20 @@
+package arrays;
+
+/**
+ * ========================================================================
+ * HINTS
+ * ========================================================================
+ *
+ * SearchSortedAndRotatedArray
+ * ---------------------------
+ * Use (mid - 1) and (mid + 1) when subdividing search intervals
+ *
+ * SolveMaze
+ * ---------
+ * Declare empty path at top of method
+ * If (start == exit), return path.add(start)
+ * If start out-of-bounds, return empty path
+ * If start is a wall, return empty path
+ * Check remainingPath non-EMPTY (rather than non-null)
+ *
+ */
