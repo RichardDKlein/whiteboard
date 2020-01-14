a7cd01ef6f336caac9975f813e401cb66ac4b6c6
Richard Klein <rklein@mobileiron.com>
2020-01-13 21:28:26 -0800

    Add hints for bit_manipulation package

---
+++ src/bit_manipulation/package-info.java
@@ -0,0 +1,15 @@
+package bit_manipulation;
+
+/**
+ * ========================================================================
+ *                                  HINTS
+ * ========================================================================
+ *
+ * BitDiff
+ * -------
+ * Xor and countOneBits()
+ *
+ * ClearBit
+ * --------
+ * Don't forget the complement operator ~
+ */
