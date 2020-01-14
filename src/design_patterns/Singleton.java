bc4cce34e0192e6e47346889850a6d4e11218a79
Richard Klein <rklein@mobileiron.com>
2020-01-13 21:58:06 -0800

    Tweak design_patterns.Singleton

--- src/design_patterns/Singleton.java
+++ src/design_patterns/Singleton.java
@@ -4,15 +4,17 @@ package design_patterns;
  * Singleton design pattern.
  */
 public class Singleton {
-    private static Singleton instance;
+    private static Singleton instance_;
+
     private Singleton() {
-        // add construction logic here
+        // construction logic
     }
-    public static Singleton instance() {
-        if (instance == null) {
-            instance = new Singleton();
+
+    public static Singleton getInstance() {
+        if (instance_ == null) {
+            instance_ = new Singleton();
         }
-        return instance;
+        return instance_;
     }
 
     static void test() {
@@ -20,8 +22,8 @@ public class Singleton {
         System.out.println("Test Singleton:");
         System.out.println("===============");
 
-        Singleton instance1 = instance();
-        Singleton instance2 = instance();
+        Singleton instance1 = getInstance();
+        Singleton instance2 = getInstance();
 
         System.out.println("1st time: instance() = "
             + instance1);
