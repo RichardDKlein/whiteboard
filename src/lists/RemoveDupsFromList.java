f956ec6c0a4af951a3ad13e295cc80f9d0ba3ddb
Richard Klein <rklein@mobileiron.com>
2020-01-14 21:00:24 -0800

    Tweak lists/RemoveDupsFromList

--- src/lists/RemoveDupsFromList.java
+++ src/lists/RemoveDupsFromList.java
@@ -1,6 +1,7 @@
 package lists;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Remove duplicates from a singly-linked list.
@@ -13,13 +14,13 @@ public final class RemoveDupsFromList {
      * @param head The list of interest.
      */
     public static <E> void removeDupsFromList(ListNode<E> head) {
-        HashSet<E> seen = new HashSet<>();
         ListNode<E> prev = null;
         ListNode<E> curr = head;
+        Set<E> seen = new HashSet<>();
         while (curr != null) {
             if (seen.contains(curr.data)) {
                 prev.next = curr.next;
-                curr = prev.next;
+                curr = curr.next;
             } else {
                 seen.add(curr.data);
                 prev = curr;
