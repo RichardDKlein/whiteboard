f02a9ae1f8d4fcaf5150a3244e3e6ccdb2c4bfed
Richard Klein <rklein@mobileiron.com>
2020-01-21 21:07:06 -0800

    Tweak strings.LongestPalindromicSubstring

--- src/strings/LongestPalindromicSubstring.java
+++ src/strings/LongestPalindromicSubstring.java
@@ -11,18 +11,16 @@ public final class LongestPalindromicSubstring {
      * Represents a closed interval [min, max] of integers.
      */
     private static class Interval {
-        public int min;
-        public int max;
-
+        int min_, max_;
         Interval(int min, int max) {
-            this.min = min;
-            this.max = max;
+            min_ = min;
+            max_ = max;
         }
         int length() {
-            return max - min + 1;
+            return max_ - min_ + 1;
         }
-        boolean valid() {
-            return (min >= 0) && (max >= 0) && (min <= max);
+        boolean isValid() {
+            return min_ >= 0 && min_ <= max_;
         }
     }
 
@@ -36,24 +34,25 @@ public final class LongestPalindromicSubstring {
     public static String longestPalindromicSubstring(String s) {
         Interval longest = new Interval(0, 0);
         for (int i = 0; i < s.length(); ++i) {
-            Interval longestOdd = longestAtCenter(s, i, true);
-            Interval longestEven = longestAtCenter(s, i, false);
-            if (longestOdd.valid() && longestOdd.length() > longest.length()) {
+            Interval longestOdd = longestCenteredAt(s, i, true);
+            Interval longestEven = longestCenteredAt(s, i, false);
+            if (longestOdd.isValid() && longestOdd.length() > longest.length()) {
                 longest = longestOdd;
             }
-            if (longestEven.valid() && longestEven.length() > longest.length()) {
+            if (longestEven.isValid() && longestEven.length() > longest.length()) {
                 longest = longestEven;
             }
         }
-        return s.substring(longest.min, longest.max + 1);
+        return s.substring(longest.min_, longest.max_ + 1);
     }
 
-    private static Interval longestAtCenter(String s, int center, boolean lengthIsOdd) {
+    private static Interval longestCenteredAt(String s, int center, boolean isLengthOdd) {
         Interval longest = new Interval(-1, -1);
-        int left = lengthIsOdd ? center - 1 : center;
+        int left = isLengthOdd ? center - 1 : center;
         int right = center + 1;
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            longest = new Interval(left--, right++);
+            longest.min_ = left--;
+            longest.max_ = right++;
         }
         return longest;
     }
--- src/strings/package-info.java
+++ src/strings/package-info.java
@@ -10,4 +10,10 @@ package strings;
  * Loop thru string from 0 to last possible index where substring could fit
  * Look for first char of substring
  * When you find it, see if the rest of the chars match
+ *
+ * LongestPalindromicSubstring
+ * ---------------------------
+ * Static inner helper class Interval
+ * For each char in string, find longest odd- and even-length palindrome centered at that char
+ * Use helper class for that
  */
