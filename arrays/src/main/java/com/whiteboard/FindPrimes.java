d966670bfb85bcdbc74a683bea472c688ea81fea
Richard Klein <rklein@mobileiron.com>
2020-03-02 20:00:52 -0800

    Tweak arrays.FindPrimes

--- arrays/src/main/java/com/whiteboard/FindPrimes.java
+++ arrays/src/main/java/com/whiteboard/FindPrimes.java
@@ -28,14 +28,16 @@ public class FindPrimes {
     public List<Integer> findPrimes(int n) {
         List<Integer> primes = new ArrayList<>();
         boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
+        for (int i = 0; i < n; ++i) {
+            isPrime[i] = true;
+        }
         isPrime[0] = false;
         isPrime[1] = false;
         for (int i = 2; i <= n; ++i) {
             if (isPrime[i]) {
                 primes.add(i);
-                for (int j = 2; j * i <= n; ++j) {
-                    isPrime[j * i] = false;
+                for (int j = 2; i * j <= n; ++j) {
+                    isPrime[i * j] = false;
                 }
             }
         }
