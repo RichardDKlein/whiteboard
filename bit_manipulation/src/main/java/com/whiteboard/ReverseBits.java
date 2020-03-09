b2887df7228409e5fba75db0a05622ed4726061e
Richard Klein <rklein@mobileiron.com>
2020-03-08 21:08:50 -0700

    Tweak bit_manipulation/ReverseBits

--- bit_manipulation/src/main/java/com/whiteboard/ReverseBits.java
+++ bit_manipulation/src/main/java/com/whiteboard/ReverseBits.java
@@ -12,14 +12,14 @@ public class ReverseBits {
      * bits reversed.
      */
     public int reverseBits(int n) {
-        int rev = 0;
+        int reverse = 0;
         int numShifts = Integer.SIZE - 1;
         for (int i = 0; i < numShifts; ++i) {
-            rev |= n & 1; // OR
-            rev <<= 1; // shift
+            reverse |= n & 1; // OR
+            reverse <<= 1; // shift
             n >>= 1;
         }
-        rev |= n & 1; // one final OR
-        return rev;
+        reverse |= n & 1; // one final OR
+        return reverse;
     }
 }
