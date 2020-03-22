35cc126ee56a67b4e0bc640376517044e2cc6a85
Richard Klein <rklein@mobileiron.com>
2020-03-22 18:07:53 -0500

    Tweak lists.ShuffleList

--- lists/src/main/java/com/whiteboard/ShuffleList.java
+++ lists/src/main/java/com/whiteboard/ShuffleList.java
@@ -19,22 +19,18 @@ public class ShuffleList<E> {
      */
     public void shuffleList(List<E> list) {
         for (int i = 0; i < list.size() - 1; ++i) {
-            int j = random(i + 1, list.size() - 1);
-            E temp = list.get(i);
-            list.set(i, list.get(j));
-            list.set(j, temp);
+            int j = randomInInterval(i + 1, list.size() - 1);
+            swapElements(list, i, j);
         }
     }
 
-    /**
-     * Return a random int in a given closed interval
-     * [min, max].
-     */
-    public static int random(int min, int max) {
-        double random0to1 = Math.random();
-        double random0toMaxMinusMin = random0to1 * (max - min);
-        int roundedRandom0toMaxMinusMin = (int)Math.round(random0toMaxMinusMin);
-        int randomMinToMax = min + roundedRandom0toMaxMinusMin;
-        return randomMinToMax;
+    int randomInInterval(int min, int max) {
+        return (int)Math.round((max - min) * Math.random() + min);
+    }
+
+    void swapElements(List<E> list, int i, int j) {
+        E tmp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, tmp);
     }
 }
