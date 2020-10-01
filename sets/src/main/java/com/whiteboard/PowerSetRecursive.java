9927aa3d9c74c19498e60c24a7433d883555265e
Richard Klein <rklein@mobileiron.com>
2020-10-01 10:03:09 -0500

    Clean up PowerSetRecursive.

--- sets/src/main/java/com/whiteboard/PowerSetRecursive.java
+++ sets/src/main/java/com/whiteboard/PowerSetRecursive.java
@@ -10,17 +10,19 @@ public class PowerSetRecursive<E> {
     public Set<Set<E>> powerSetRecursive(Set<E> set) {
         Set<Set<E>> powerSet = new HashSet<>();
         if (set.isEmpty()) {
-            powerSet.add(new HashSet<>());
+            Set<E> emptySet = new HashSet<>();
+            powerSet.add(emptySet);
             return powerSet;
         }
         List<E> elements = new ArrayList<>(set);
         E firstElement = elements.remove(0);
-        Set<E> remainder = new HashSet<>(elements);
-        Set<Set<E>> remainderPowerSet = powerSetRecursive(remainder);
-        for (Set<E> subset : remainderPowerSet) {
-            powerSet.add(new HashSet<>(subset));
-            subset.add(firstElement);
-            powerSet.add(subset);
+        Set<E> remainderSet = new HashSet<>(elements);
+        Set<Set<E>> remainderPowerSet = powerSetRecursive(remainderSet);
+        for (Set<E> remainderSubset : remainderPowerSet) {
+            powerSet.add(remainderSubset);
+            Set<E> copyOfRemainderSubset = new HashSet<>(remainderSubset);
+            copyOfRemainderSubset.add(firstElement);
+            powerSet.add(copyOfRemainderSubset);
         }
         return powerSet;
     }
