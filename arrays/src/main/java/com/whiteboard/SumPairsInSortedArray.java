3fbcf896d4940ac3a47018a8be6db13a370a9860
Richard Klein <rklein@mobileiron.com>
2020-10-06 16:40:06 -0500

    Cleanup SumPairsInSortedArray

--- arrays/src/main/java/com/whiteboard/SumPairsInSortedArray.java
+++ arrays/src/main/java/com/whiteboard/SumPairsInSortedArray.java
@@ -1,7 +1,5 @@
 package com.whiteboard;
 
-import com.sun.tools.javac.util.Pair;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,7 +7,34 @@ import java.util.Set;
  * In a sorted (in increasing order) array, find
  * pairs of array elements that sum to a given value.
  */
-public class SumPairsInSortedArray {
+public final class SumPairsInSortedArray {
+    private SumPairsInSortedArray() {
+    }
+
+    static class Pair {
+        int first;
+        int second;
+
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        // Following methods are needed only for the unit test.
+        @Override
+        public int hashCode() {
+            return (first << 15) + second;
+        }
+        @Override
+        public boolean equals(Object other) {
+            if (!(other instanceof Pair)) {
+                return false;
+            }
+            Pair pair = (Pair) other;
+            return this.first == pair.first && this.second == pair.second;
+        }
+    }
+
     /**
      * In a sorted (in increasing order) array, find
      * pairs of array elements that sum to a given value.
@@ -23,9 +48,8 @@ public class SumPairsInSortedArray {
      * @param a The sorted array to search for pairs.
      * @param sum The value to which each pair of elements should sum.
      */
-    public Set<Pair<Integer, Integer>>
-    sumPairsInSortedArray(int[] a, int sum) {
-        Set<Pair<Integer, Integer>> pairs = new HashSet<>();
+    public static Set<Pair> sumPairsInSortedArray(int[] a, int sum) {
+        Set<Pair> pairs = new HashSet<>();
         int left = 0;
         int right = a.length - 1;
         while (left < right) {
@@ -34,7 +58,7 @@ public class SumPairsInSortedArray {
             } else if (a[left] + a[right] > sum) {
                 --right;
             } else {
-                pairs.add(new Pair<>(a[left], a[right]));
+                pairs.add(new Pair(a[left], a[right]));
                 ++left;
                 --right;
             }
--- arrays/src/test/java/com/whiteboard/SumPairsInSortedArrayTest.java
+++ arrays/src/test/java/com/whiteboard/SumPairsInSortedArrayTest.java
@@ -1,10 +1,10 @@
 package com.whiteboard;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-
-import com.sun.tools.javac.util.Pair;
 import org.junit.jupiter.api.Test;
+import com.whiteboard.SumPairsInSortedArray.Pair;
 
 class SumPairsInSortedArrayTest {
     @Test
@@ -14,16 +14,23 @@ class SumPairsInSortedArrayTest {
         System.out.println("===========================");
 
         int a[] = {-2, -1, 0, 3, 5, 6, 6, 7, 9, 13, 14};
+
+        Set<Pair> expected = new HashSet<>();
+        expected.add(new Pair(6, 6));
+        expected.add(new Pair(-1, 13));
+        expected.add(new Pair(5, 7));
+        expected.add(new Pair(3, 9));
+        expected.add(new Pair(-2, 14));
+
         System.out.println("sorted array = " + Arrays.toString(a));
 
         int sum = 12;
-        Set<Pair<Integer, Integer>> pairs = new SumPairsInSortedArray()
-                .sumPairsInSortedArray(a, sum);
+        Set<Pair> pairs = SumPairsInSortedArray.sumPairsInSortedArray(a, sum);
+        assert(pairs.equals(expected));
 
-        for (Pair<Integer, Integer> pair : pairs) {
-            int p1 = pair.fst;
-            int p2 = pair.snd;
-            assert(p1 + p2 == sum);
+        for (Pair pair : pairs) {
+            int p1 = pair.first;
+            int p2 = pair.second;
             System.out.println(p1 + " + " + p2 + " = " + (p1 + p2));
         }
     }
