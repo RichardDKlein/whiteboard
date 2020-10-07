58f32df401888d4f57a1e3e4237eb2dbb4b877ef
Richard Klein <rklein@mobileiron.com>
2020-10-06 23:59:41 -0500

    Cleanup SpiralTraverse2dArray

--- arrays/src/main/java/com/whiteboard/SpiralTraverse2dArray.java
+++ arrays/src/main/java/com/whiteboard/SpiralTraverse2dArray.java
@@ -8,7 +8,10 @@ import java.util.List;
  * the upper-left element and spiralling in the clockwise
  * direction.
  */
-public class SpiralTraverse2dArray {
+public final class SpiralTraverse2dArray {
+    private SpiralTraverse2dArray() {
+    }
+
     /**
      * Traverse a 2D array, NOT NECESSARILY SQUARE, in a spiral
      * pattern, starting at the upper-left element, and spiralling
@@ -25,53 +28,53 @@ public class SpiralTraverse2dArray {
      *          pattern.
      * @return A list containing the array elements in spiral order.
      */
-    public List<Integer> spiralTraverse2dArray(int[][] a) {
-        List<Integer> spiral = new ArrayList<>();
+    public static List<Integer> spiralTraverse2dArray(int[][] a) {
+        List<Integer> result = new ArrayList<>();
         for (int rowTop = 0, rowBottom = a.length - 1,
-                colLeft = 0, colRight = a[0].length - 1;
+             colLeft = 0, colRight = a[0].length - 1;
              rowTop <= rowBottom && colLeft <= colRight;
              ++rowTop, --rowBottom, ++colLeft, --colRight) {
 
             List<Integer> shell = spiralTraverseShell(
                     a, rowTop, rowBottom, colLeft, colRight);
-            spiral.addAll(shell);
+            result.addAll(shell);
         }
-        return spiral;
+        return result;
     }
 
-    private List<Integer> spiralTraverseShell(int[][]a,
+    private static List<Integer> spiralTraverseShell(int[][]a,
             int rowTop, int rowBottom, int colLeft, int colRight) {
         // degenerate case: horizontal vector
-        List<Integer> shell = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         if (rowTop == rowBottom) {
             for (int col = colLeft; col <= colRight; ++col) {
-                shell.add(a[rowTop][col]);
+                result.add(a[rowTop][col]);
             }
-            return shell;
+            return result;
         }
         // degenerate case: vertical vector
         if (colLeft == colRight) {
             for (int row = rowTop; row <= rowBottom; ++row) {
-                shell.add(a[row][colLeft]);
+                result.add(a[row][colLeft]);
             }
-            return shell;
+            return result;
         }
         // top row of shell
         for (int col = colLeft; col < colRight; ++col) {
-            shell.add(a[rowTop][col]);
+            result.add(a[rowTop][col]);
         }
         // right column of shell
         for (int row = rowTop; row < rowBottom; ++row) {
-            shell.add(a[row][colRight]);
+            result.add(a[row][colRight]);
         }
         // bottom row of shell
         for (int col = colRight; col > colLeft; --col) {
-            shell.add(a[rowBottom][col]);
+            result.add(a[rowBottom][col]);
         }
         // left column of shell
         for (int row = rowBottom; row > rowTop; --row) {
-            shell.add(a[row][colLeft]);
+            result.add(a[row][colLeft]);
         }
-        return shell;
+        return result;
     }
 }
--- arrays/src/test/java/com/whiteboard/SpiralTraverse2dArrayTest.java
+++ arrays/src/test/java/com/whiteboard/SpiralTraverse2dArrayTest.java
@@ -1,5 +1,6 @@
 package com.whiteboard;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -77,14 +78,37 @@ class SpiralTraverse2dArrayTest {
                         {25, 26, 27, 28}
                 }
         };
+
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1));
+        expected.add(Arrays.asList(1, 2, 3, 4));
+        expected.add(Arrays.asList(1, 2, 3, 4));
+        expected.add(Arrays.asList(1, 2, 4, 3));
+        expected.add(Arrays.asList(1, 2, 3, 4, 8, 7, 6, 5));
+        expected.add(Arrays.asList(1, 2, 4, 6, 8, 7, 5, 3));
+        expected.add(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5));
+        expected.add(Arrays.asList(1, 2, 3, 4, 5, 10, 15, 14, 13,
+                12, 11, 6, 7, 8, 9));
+        expected.add(Arrays.asList(1, 2, 3, 6, 9, 12, 15, 14, 13,
+                10, 7, 4, 5, 8, 11));
+        expected.add(Arrays.asList(1, 2, 3, 4, 8, 12, 16, 15, 14,
+                13, 9, 5, 6, 7, 11, 10));
+        expected.add(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 14, 21,
+                28, 27, 26, 25, 24, 23, 22, 15, 8, 9, 10, 11, 12,
+                13, 20, 19, 18, 17, 16));
+        expected.add(Arrays.asList(1, 2, 3, 4, 8, 12, 16, 20, 24,
+                28, 27, 26, 25, 21, 17, 13, 9, 5, 6, 7, 11, 15, 19,
+                23, 22, 18, 14, 10));
+
         for (int i = 0; i < a.length; ++i) {
             System.out.println("\nArray:");
             int rows = a[i].length;
             for (int row = 0; row < rows; ++row) {
                 System.out.println(Arrays.toString(a[i][row]));
             }
-            List<Integer> spiral = new SpiralTraverse2dArray()
+            List<Integer> spiral = SpiralTraverse2dArray
                     .spiralTraverse2dArray(a[i]);
+            assert(spiral.equals(expected.get(i)));
             System.out.println("\nSpiral Traversal:");
             System.out.println(spiral.toString());
         }
