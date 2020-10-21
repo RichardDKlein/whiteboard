1bdf224856fb94bc6f0f10f387a728666c5e23f7
Richard Klein <rklein@mobileiron.com>
2020-10-21 09:49:11 -0500

    Cleanup Singleton

--- design_patterns/src/main/java/com/whiteboard/Singleton.java
+++ design_patterns/src/main/java/com/whiteboard/Singleton.java
@@ -13,7 +13,9 @@ public class Singleton {
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
-                instance = new Singleton();
+                if (instance == null) {
+                    instance = new Singleton();
+                }
             }
         }
         return instance;
