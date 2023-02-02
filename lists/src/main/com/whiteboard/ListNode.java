cd4a8260779edb73520cc8ff7325b7ff66383a20
RichardDKlein <RichardDKlein@gmail.com>
2023-02-01 18:11:51 -0600

    Practicing lists

--- lists/src/main/com/whiteboard/ListNode.java
+++ lists/src/main/com/whiteboard/ListNode.java
@@ -7,4 +7,17 @@ public class ListNode<E> {
     ListNode(E data) {
         this.data = data;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ListNode<?> other)) {
+            return false;
+        }
+        return this.data.equals(other.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return data.hashCode();
+    }
 }
