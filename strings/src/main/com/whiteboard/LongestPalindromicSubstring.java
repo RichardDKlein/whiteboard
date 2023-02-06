cc84af46eff29643111d3e437ce6d873c76feb61
RichardDKlein <RichardDKlein@gmail.com>
2023-02-06 09:12:11 -0600

    Practicing LongestPalindromicSubstring

--- strings/src/main/com/whiteboard/LongestPalindromicSubstring.java
+++ strings/src/main/com/whiteboard/LongestPalindromicSubstring.java
@@ -18,34 +18,21 @@ public final class LongestPalindromicSubstring {
     public static String longestPalindromicSubstring(String s) {
         String result = s.substring(0, 1);
         for (int i = 0; i < s.length() - 1; ++i) {
-            String longestEven = longestWithEvenLengthCenteredAt(s, i, i + 1);
-            if (longestEven.length() > result.length()) {
-                result = longestEven;
+            String longestWithEvenLength = longestCenteredAt(s, i, i + 1);
+            if (longestWithEvenLength.length() > result.length()) {
+                result = longestWithEvenLength;
             }
         }
         for (int i = 1; i < s.length() - 1; ++i) {
-            String longestOdd = longestWithOddLengthCenteredAt(s, i);
-            if (longestOdd.length() > result.length()) {
-                result = longestOdd;
+            String longestWithOddLength = longestCenteredAt(s, i - 1, i + 1);
+            if (longestWithOddLength.length() > result.length()) {
+                result = longestWithOddLength;
             }
         }
         return result;
     }
 
-    private static String longestWithEvenLengthCenteredAt(String s, int left, int right) {
-        return longestCenteredAt(s, left, right);
-    }
-
-    private static String longestWithOddLengthCenteredAt(String s, int center) {
-        int left = center - 1;
-        int right = center + 1;
-        return longestCenteredAt(s, left, right);
-    }
-
     private static String longestCenteredAt(String s, int left, int right) {
-        if (s.charAt(left) != s.charAt(right)) {
-            return "";
-        }
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             --left;
             ++right;
