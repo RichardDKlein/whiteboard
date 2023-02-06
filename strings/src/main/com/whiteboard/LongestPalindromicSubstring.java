8e43512bb5462524d391780a7dea3aaafe26897e
RichardDKlein <RichardDKlein@gmail.com>
2023-02-06 09:36:17 -0600

    Practicing LongestPalindromicSubstring

--- strings/src/main/com/whiteboard/LongestPalindromicSubstring.java
+++ strings/src/main/com/whiteboard/LongestPalindromicSubstring.java
@@ -17,7 +17,7 @@ public final class LongestPalindromicSubstring {
      */
     public static String longestPalindromicSubstring(String s) {
         String result = s.substring(0, 1);
-        for (int i = 0; i < s.length() - 1; ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             String longestWithEvenLength = longestCenteredAt(s, i, i + 1);
             String longestWithOddLength = longestCenteredAt(s, i - 1, i + 1);
             if (longestWithEvenLength.length() > result.length()) {
