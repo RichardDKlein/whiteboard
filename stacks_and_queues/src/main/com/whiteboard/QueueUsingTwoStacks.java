34fb22834ad10eba9063219329eec9186a68e7e6
RichardDKlein <RichardDKlein@gmail.com>
2023-03-03 14:44:35 -0600

    Practice QueueUsingTwoStacks

--- stacks_and_queues/src/main/com/whiteboard/QueueUsingTwoStacks.java
+++ stacks_and_queues/src/main/com/whiteboard/QueueUsingTwoStacks.java
@@ -6,19 +6,19 @@ import java.util.Stack;
  * Implement a queue using two stacks.
  */
 public class QueueUsingTwoStacks<E> {
-    private Stack<E> addStack = new Stack<>();
-    private Stack<E> removeStack = new Stack<>();
+    Stack<E> addStack = new Stack<>();
+    Stack<E> pollStack = new Stack<>();
 
-    public void add(E element) {
-        addStack.push(element);
+    public void add(E item) {
+        addStack.push(item);
     }
 
     public E poll() {
-        if (removeStack.isEmpty()) {
+        if (pollStack.isEmpty()) {
             while (!addStack.isEmpty()) {
-                removeStack.push(addStack.pop());
+                pollStack.push(addStack.pop());
             }
         }
-        return removeStack.isEmpty() ? null : removeStack.pop();
+        return pollStack.isEmpty() ? null : pollStack.pop();
     }
 }
