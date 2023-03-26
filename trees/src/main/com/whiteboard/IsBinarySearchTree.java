10e99948ab0835bc1f276bd2f17e43492cf4cf38
RichardDKlein <RichardDKlein@gmail.com>
2023-03-26 14:46:03 -0500

    Practice IsBinarySearchTree

--- trees/src/main/com/whiteboard/IsBinarySearchTree.java
+++ trees/src/main/com/whiteboard/IsBinarySearchTree.java
@@ -39,8 +39,8 @@ public final class IsBinarySearchTree {
     }
 
     private static boolean isAscending(List<Integer> list) {
-        for (int i = 0; i < list.size() - 1; ++i) {
-            if (list.get(i + 1) < list.get(i)) {
+        for (int i = 1; i < list.size(); ++i) {
+            if (list.get(i) < list.get(i - 1)) {
                 return false;
             }
         }
