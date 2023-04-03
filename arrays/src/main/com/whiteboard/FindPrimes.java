17161b432c9ea90cd6a465cec3cf165c330be730
RichardDKlein <RichardDKlein@gmail.com>
2023-04-03 09:10:31 -0500

    Practice FindPrimes

--- arrays/src/main/com/whiteboard/FindPrimes.java
+++ arrays/src/main/com/whiteboard/FindPrimes.java
@@ -36,13 +36,13 @@ public final class FindPrimes {
         boolean[] isPrime = new boolean[n + 1];
         isPrime[0] = false;
         isPrime[1] = false;
-        for (int i = 2; i < isPrime.length; ++i) {
+        for (int i = 2; i <= n; ++i) {
             isPrime[i] = true;
         }
-        for (int i = 2; i < isPrime.length; ++i) {
+        for (int i = 2; i <= n; ++i) {
             if (isPrime[i]) {
                 result.add(i);
-                for (int j = 2; i * j < isPrime.length; ++j) {
+                for (int j = 2; i * j <= n; ++j) {
                     isPrime[i * j] = false;
                 }
             }
