e02e7b77459a7d16ad2f648f21289bc9957a8475
RichardDKlein <RichardDKlein@gmail.com>
2023-04-12 17:48:03 -0500

    Add ReverseString

---
+++ strings/src/main/com/whiteboard/ReverseString.java
@@ -0,0 +1,31 @@
+package com.whiteboard;
+
+import java.util.Arrays;
+
+/**
+ * Reverse a given string.
+ */
+public final class ReverseString {
+    private ReverseString() {
+    }
+
+    /**
+     * Reverse the given string.
+     *
+     * @param s The string to reverse.
+     * @return A reversed copy of the given string.
+     */
+    public static String reverseString(String s) {
+        char[] buf = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            char tmp = buf[left];
+            buf[left] = buf[right];
+            buf[right] = tmp;
+            ++left;
+            --right;
+        }
+        return new String(buf);
+    }
+}
