4aecb02608f37775e06910e5d6c85c8d4d78dd7a
RichardDKlein <RichardDKlein@gmail.com>
2023-10-22 15:36:24 -0500

    Add test case

--- math/src/test/com/whiteboard/java/ReverseDecimalNumberTest.java
+++ math/src/test/com/whiteboard/java/ReverseDecimalNumberTest.java
@@ -9,8 +9,8 @@ public class ReverseDecimalNumberTest {
         System.out.println("Test ReverseDecimalNumber:");
         System.out.println("==========================");
 
-        int[] n        = {0, 1, 12, 123, 1234, 12345, 123456, 1234567, 12345678};
-        int[] expected = {0, 1, 21, 321, 4321, 54321, 654321, 7654321, 87654321};
+        int[] n        = {0, 1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789};
+        int[] expected = {0, 1, 21, 321, 4321, 54321, 654321, 7654321, 87654321, 987654321};
 
         for (int i = 0; i < n.length; ++i) {
             int reversed = ReverseDecimalNumber.reverseDecimalNumber(n[i]);
