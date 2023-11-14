2aa561a3f3aad3a574f4f4e20022ea5bdb43a983
RichardDKlein <RichardDKlein@gmail.com>
2023-11-13 19:29:55 -0600

    Tweak InsertBitsTest

--- bit_manipulation/src/test/com/whiteboard/java/InsertBitsTest.java
+++ bit_manipulation/src/test/com/whiteboard/java/InsertBitsTest.java
@@ -9,14 +9,14 @@ public class InsertBitsTest {
         System.out.println("Test InsertBits:");
         System.out.println("================");
 
-        int expected = 0b00111111000000000000000000000000;
+        int expected = 0b11101010111111111111111111111111;
 
-        int n = 0;
+        int n = -1;
 
         System.out.println("Destination bits:");
         BitTestUtils.printBits(n);
         System.out.println("Bits to insert:");
-        int bits = 0x3F;
+        int bits = 0x2a;
         BitTestUtils.printBits(bits);
         n = InsertBits.insertBits(n, bits, 29, 24);
         assert(n == expected);
