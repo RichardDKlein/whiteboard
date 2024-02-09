c16a744edd3ee950e57ba1f7ae10e047ca281bbf
RichardDKlein <RichardDKlein@gmail.com>
2024-02-08 21:21:36 -0600

    Tweak the Javadoc

--- arrays/src/main/com/whiteboard/java/CircularQueue.java
+++ arrays/src/main/com/whiteboard/java/CircularQueue.java
@@ -12,10 +12,10 @@ package com.whiteboard.java;
  * The queue is considered empty when the head and the tail
  * point to the same element. The queue is considered full
  * when the next element after the tail is the head. Since
- * the tail always points to the next free slot, a full
- * queue contains exactly one free, and unusable, slot.
- * Thus, the capacity of a buffer of length n is actually
- * (n - 1).
+ * the tail always points to the next free slot, a full queue
+ * contains exactly one free, and unusable, slot. Thus, if we
+ * want the queue to be able to hold `capacity` elements, we
+ * need to allocate a buffer of length (capacity + 1).
  */
 public class CircularQueue {
     private int[] buf;
