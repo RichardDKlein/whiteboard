d5f2856b8242c367b3d0a75880ed674b56d83d4a
RichardDKlein <RichardDKlein@gmail.com>
2024-02-16 18:58:12 -0600

    Cosmetic

--- strings/src/test/com/whiteboard/java/StringTestUtils.java
+++ strings/src/test/com/whiteboard/java/StringTestUtils.java
@@ -14,9 +14,11 @@ public class StringTestUtils {
     public static void printLongString(String s) {
         int maxLen = 68;
         int currIndex = 0;
+
         while (currIndex < s.length()) {
             int nextIndex = Math.min(currIndex + maxLen, s.length());
             String line = s.substring(currIndex, nextIndex);
+
             if (nextIndex < s.length() && s.charAt(nextIndex) != ' ') {
                 int lastSpace = line.lastIndexOf(' ');
                 if (lastSpace != -1) {
@@ -24,6 +26,7 @@ public class StringTestUtils {
                     line = s.substring(currIndex, nextIndex);
                 }
             }
+
             System.out.println(line);
             currIndex = nextIndex;
         }
