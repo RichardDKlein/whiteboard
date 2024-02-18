0cee9abe15dce6e9aa13e62cb9cbbcbee3e49a09
RichardDKlein <RichardDKlein@gmail.com>
2024-02-18 16:21:14 -0600

    Tweak FindPrimes

--- arrays/src/main/com/whiteboard/java/FindPrimes.java
+++ arrays/src/main/com/whiteboard/java/FindPrimes.java
@@ -11,18 +11,18 @@ public class FindPrimes {
      * to keep track of which integers are prime: `isPrime[i]` = `true` if
      * and only if 'i' is prime.
      * <p>
-     * All elements of `isPrime` (except `isPrime[0]` and `isPrime[1]) are
-     * initialized to `true`, since as far as we know at the outset, all
-     * integers (except 0 and 1) are prime.
+     * The `isPrime` array is initialized as follows: `isPrime[2]` is set
+     * to `true`, since we know that 2 is prime. In addition, `isPrime[i]
+     * is set to `true` for all odd `i` > 1, since as far as we know at the
+     * outset, all odd integers > 1, as well as 2, are prime. `isPrime[1]`,
+     * as well as `isPrime[i] for all even `i` > 2, are set to `false`.
      * <p>
-     * We then loop through the `isPrime` array. If `isPrime[i]` is `true`,
-     * we mark all multiples of `i` as composite (i.e., non-prime). We do
-     * this by setting `isPrime[m * i]` to `false` for each multiple of `i`.
-     * If `isPrime[i]` is `false`, we just go on to the next element.
-     * <p>
-     * When setting `isPrime[m * i]` to `false` for each multiple of `i`,
-     * we can take advantage of the following optimization: We need not
-     * start from `m` = 2; instead, we can start from `m` = `i`.
+     * We then loop through the odd indices of the `isPrime` array. If
+     * `isPrime[i]` is `true`, we mark all multiples of `i` as composite
+     * (i.e., non-prime). We do this by setting `isPrime[m * i]` to `false`
+     * for each multiple of `i`. Here, we take advantage of the following
+     * optimization: We need not start from `m` = 2; instead, we can start
+     * from `m` = `i`.
      * <p>
      * To see why this is true, suppose that `m < i`. Now, `m` has a prime
      * factorization `p1 * p2 * ... * pN`, where `pI <= m < i` for each of
@@ -38,13 +38,13 @@ public class FindPrimes {
      */
     public static List<Integer> findPrimes(int n) {
         List<Integer> result = new ArrayList<>();
-        boolean[] isPrime = new boolean[n + 1];
-        isPrime[0] = isPrime[1] = false;
-        if (n >= 2) {
-            isPrime[2] = true;
+        if (n < 2) {
+            return result;
         }
-        for (int i = 3; i <= n; i++) {
-            isPrime[i] = i % 2 != 0;
+        boolean[] isPrime = new boolean[n + 1];
+        isPrime[2] = true;
+        for (int i = 3; i <= n; i += 2) {
+            isPrime[i] = true;
         }
         for (int i = 3; i * i <= n; i += 2) {
             if (isPrime[i]) {
