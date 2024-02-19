ba573fb0e21c1f8028f6b80521b79087871df9fa
RichardDKlein <RichardDKlein@gmail.com>
2024-02-19 10:42:05 -0600

    Tweak FindPrimes

--- arrays/src/main/com/whiteboard/java/FindPrimes.java
+++ arrays/src/main/com/whiteboard/java/FindPrimes.java
@@ -5,25 +5,22 @@ import java.util.List;
 
 public class FindPrimes {
     /**
-     * Find all primes up to a given integer.
-     * <p>
+     * Find all primes up to a given integer.</p>
+     *
      * Our algorithm is the Sieve of Eratosthenes. We use an array 'isPrime'
      * to keep track of which integers are prime: `isPrime[i]` = `true` if
-     * and only if 'i' is prime.
-     * <p>
-     * The `isPrime` array is initialized as follows: `isPrime[2]` is set
-     * to `true`, since we know that 2 is prime. In addition, `isPrime[i]
-     * is set to `true` for all odd `i` > 1, since as far as we know at the
-     * outset, all odd integers > 1, as well as 2, are prime. `isPrime[1]`,
-     * as well as `isPrime[i] for all even `i` > 2, are set to `false`.
-     * <p>
-     * We then loop through the odd indices of the `isPrime` array. If
-     * `isPrime[i]` is `true`, we mark all multiples of `i` as composite
-     * (i.e., non-prime). We do this by setting `isPrime[m * i]` to `false`
-     * for each multiple of `i`. Here, we take advantage of the following
-     * optimization: We need not start from `m` = 2; instead, we can start
-     * from `m` = `i`.
-     * <p>
+     * and only if 'i' is prime.</p>
+     *
+     * All elements of `isPrime` are initialized to `true`, since as far as
+     * we know at the outset, all positive integers are prime.</p>
+     *
+     * We then loop through the `isPrime` array, from `i` = 2` to the square
+     * root of `n`. If `isPrime[i]` is `true`, we mark all multiples of `i`
+     * as composite (i.e., non-prime). We do this by setting `isPrime[m * i]`
+     * to `false` for each multiple of `i`. Here, we take advantage of the
+     * following optimization: We need not start from `m` = 2; instead, we
+     * can start from `m` = `i`.</p>
+     *
      * To see why this is true, suppose that `m < i`. Now, `m` has a prime
      * factorization `p1 * p2 * ... * pN`, where `pI <= m < i` for each of
      * the prime factors `pI`. In particular, `p1 < i`. But that means that
@@ -33,20 +30,16 @@ public class FindPrimes {
      * marked all multiples of `p1` as composite.
      *
      * @param n A positive integer.
-     * @return A list containing all primes up to and including the
-     * given integer.
+     * @return A list containing all primes up to and including `n`.
      */
     public static List<Integer> findPrimes(int n) {
         List<Integer> result = new ArrayList<>();
-        if (n < 2) {
-            return result;
-        }
         boolean[] isPrime = new boolean[n + 1];
-        isPrime[2] = true;
-        for (int i = 3; i <= n; i += 2) {
+        for (int i = 0; i <= n; i++) {
             isPrime[i] = true;
         }
-        for (int i = 3; i * i <= n; i += 2) {
+        int sqrtN = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrtN; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
                     isPrime[j] = false;
