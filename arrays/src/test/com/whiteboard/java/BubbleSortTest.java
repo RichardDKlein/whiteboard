c3459c1e8f5775c932a9a84d44e469a36671a2ba
RichardDKlein <RichardDKlein@gmail.com>
2024-04-05 14:34:27 -0500

    Remove unnecessary `new int[]`

--- arrays/src/test/com/whiteboard/java/BubbleSortTest.java
+++ arrays/src/test/com/whiteboard/java/BubbleSortTest.java
@@ -20,36 +20,36 @@ public class BubbleSortTest {
 
     @Test
     public void arrayWithOneElement() {
-        int[] a = new int[] {1};
-        int[] expected = new int[] {1};
+        int[] a = {1};
+        int[] expected = {1};
         doTest(a, expected);
     }
 
     @Test
     public void alreadySortedArray() {
-        int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         doTest(a, expected);
     }
 
     @Test
     public void worstCase() {
-        int[] a = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         doTest(a, expected);
     }
 
     @Test
     public void averageCase1() {
-        int[] a = new int[] {2, 1, 4, 3, 6, 5, 8, 7, 10, 9};
-        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a = {2, 1, 4, 3, 6, 5, 8, 7, 10, 9};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         doTest(a, expected);
     }
 
     @Test
     public void averageCase2() {
-        int[] a = new int[] {4, 2, 3, 1, 7, 5, 10, 9, 8, 6};
-        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a = {4, 2, 3, 1, 7, 5, 10, 9, 8, 6};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         doTest(a, expected);
     }
 
