ffb2a2132d64ebadb05948b8c647d46e26893fd6
RichardDKlein <RichardDKlein@gmail.com>
2024-04-11 19:44:54 -0500

    Tweak SpiralTraverse2dArray

--- arrays/src/main/com/whiteboard/java/SpiralTraverse2dArray.java
+++ arrays/src/main/com/whiteboard/java/SpiralTraverse2dArray.java
@@ -24,46 +24,46 @@ public final class SpiralTraverse2dArray {
         List<Integer> result = new ArrayList<>();
         int numRows = a.length;
         int numCols = a[0].length;
-        for (int topRow = 0, bottomRow = numRows - 1, leftCol = 0, rightCol = numCols - 1;
+        for (int topRow = 0, rightCol = numCols - 1, bottomRow = numRows - 1, leftCol = 0;
              topRow <= bottomRow && leftCol <= rightCol;
-             ++topRow, --bottomRow, ++leftCol, --rightCol) {
+             topRow++, rightCol--, bottomRow--, leftCol++) {
 
-            result.addAll(spiralTraverseShell(a, topRow, bottomRow, leftCol, rightCol));
+            result.addAll(spiralTraverseShell(a, topRow, rightCol, bottomRow, leftCol));
         }
         return result;
     }
 
-    private static List<Integer> spiralTraverseShell(int[][] a, int topRow, int bottomRow,
-                                                     int leftCol, int rightCol) {
+    private static List<Integer> spiralTraverseShell(int[][] a, int topRow, int rightCol,
+                                                     int bottomRow, int leftCol) {
         List<Integer> result = new ArrayList<>();
         // degenerate case: row vector
         if (topRow == bottomRow) {
-            for (int col = leftCol; col <= rightCol; ++col) {
+            for (int col = leftCol; col <= rightCol; col++) {
                 result.add(a[topRow][col]);
             }
             return result;
         }
         // degenerate case: column vector
         if (leftCol == rightCol) {
-            for (int row = topRow; row <= bottomRow; ++row) {
+            for (int row = topRow; row <= bottomRow; row++) {
                 result.add(a[row][leftCol]);
             }
             return result;
         }
-        // top row: left to right
-        for (int col = leftCol; col < rightCol; ++col) {
+        // top row
+        for (int col = leftCol; col < rightCol; col++) {
             result.add(a[topRow][col]);
         }
-        // right col: top to bottom
-        for (int row = topRow; row < bottomRow; ++row) {
+        // right column
+        for (int row = topRow; row < bottomRow; row++) {
             result.add(a[row][rightCol]);
         }
-        // bottom row: right to left
-        for (int col = rightCol; col > leftCol; --col) {
+        // bottom row
+        for (int col = rightCol; col > leftCol; col--) {
             result.add(a[bottomRow][col]);
         }
-        // left col: bottom to top
-        for (int row = bottomRow; row > topRow; --row) {
+        // left column
+        for (int row = bottomRow; row > topRow; row--) {
             result.add(a[row][leftCol]);
         }
         return result;
